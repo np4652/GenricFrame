677bcbb21dfda47ae85e822cabf39f7cc8cf41b4
Amit Singh <np4652@gmail.com>
2022-01-11 15:53:51 +0530

    done

--- GenricFrame/AppCode/Migrations/InitialTables_202106280001.cs
+++ GenricFrame/AppCode/Migrations/InitialTables_202106280001.cs
@@ -17,6 +17,68 @@ namespace GenricFrame.AppCode.Migrations
 
         public override void Up()
         {
+            /* Role */
+            Create.Table("IdentityRole")
+               .WithColumn("Id").AsInt64().NotNullable().Identity().PrimaryKey()
+               .WithColumn("ConcurrencyStamp").AsString(1000).NotNullable()
+               .WithColumn("Name").AsString(256).NotNullable()
+               .WithColumn("NormalizedName").AsString(256).NotNullable()
+               .WithColumn("Country").AsString(50).NotNullable();
+            /* Role Claim */
+            Create.Table("RoleClaims")
+               .WithColumn("Id").AsInt64().NotNullable().Identity()
+               .WithColumn("ClaimType").AsString(1000).NotNullable()
+               .WithColumn("ClaimValue").AsString(1000).NotNullable()
+               .WithColumn("RoleId").AsInt64().NotNullable()
+               .WithColumn("Country").AsString(50).NotNullable();
+
+            /* Users */
+            Create.Table("Users")
+               .WithColumn("Id").AsString(1000).NotNullable()
+               .WithColumn("AccessFailedCount").AsInt64()
+               .WithColumn("ConcurrencyStamp").AsString(1000)
+               .WithColumn("Email").AsString(256).NotNullable()
+               .WithColumn("EmailConfirmed").AsBoolean().NotNullable()
+               .WithColumn("LockoutEnabled").AsBoolean()
+               .WithColumn("LockoutEnd").AsDateTime()
+               .WithColumn("NormalizedEmail").AsString(256).NotNullable()
+               .WithColumn("NormalizedUserName").AsString(256).NotNullable()
+               .WithColumn("PasswordHash").AsString(256).NotNullable()
+               .WithColumn("PhoneNumberConfirmed").AsBoolean().NotNullable()
+               .WithColumn("SecurityStamp").AsString(1000).NotNullable()
+               .WithColumn("TwoFactorEnabled").AsBoolean().NotNullable()
+               .WithColumn("UserName").AsString(256).NotNullable();
+
+            /* UserClaims */
+            Create.Table("UserClaims")
+               .WithColumn("Id").AsInt64().Identity().NotNullable()
+               .WithColumn("ClaimType").AsString(1000)
+               .WithColumn("ClaimValue").AsString(1000)
+               .WithColumn("UserId").AsString(256).NotNullable();
+
+            /* UserLogins */
+            Create.Table("UserLogins")
+               .WithColumn("Id").AsInt64().Identity().NotNullable()
+               .WithColumn("LoginProvider").AsString(128)
+               .WithColumn("ProviderKey").AsString(128)
+               .WithColumn("ProviderDisplayName").AsString(1000)
+               .WithColumn("UserId").AsString(256).NotNullable();
+
+            /* UserTokens */
+            Create.Table("UserTokens")
+               .WithColumn("Id").AsInt64().Identity().NotNullable()
+               .WithColumn("UserId").AsString(256).NotNullable()
+               .WithColumn("LoginProvider").AsString(128).NotNullable()
+               .WithColumn("Name").AsString(128).NotNullable()
+               .WithColumn("Value").AsString(128).NotNullable();
+
+            /* UserRoles */
+            Create.Table("UserRoles")
+               .WithColumn("Id").AsInt64().Identity().NotNullable()
+               .WithColumn("UserId").AsString(256).NotNullable()
+               .WithColumn("RoleId").AsString(128).NotNullable();
+
+
             Create.Table("Companies")
                 .WithColumn("Id").AsInt64().NotNullable().Identity()
                 .WithColumn("CompanyId").AsGuid().NotNullable().PrimaryKey()
